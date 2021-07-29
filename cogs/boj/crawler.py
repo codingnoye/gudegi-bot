0574dfda0cb5eeaa95ca5f1cbf092a06ad42656a
CodingNoye <codingnoye@kookmin.ac.kr>
2021-07-29 23:36:05 +0900

    아마도 버그 수정?

--- cogs/boj/crawler.py
+++ cogs/boj/crawler.py
@@ -19,15 +19,18 @@ class Crawler:
             for handle in self.users:
                 user: User = self.users[handle]
                 userdata = await getUserData(user.handle)
+                if userdata is None: continue
+                print(f'{user.handle}: solved {userdata["solvedCount"]} problems')
                 if userdata['solvedCount'] != user.solved_count:
                     solved_problems = await getSolvedProblems(user.handle)
+                    if len(solved_problems) == 0: continue
                     for prob_num in solved_problems^user.solved_problems:
                         await self.callback(user, prob_num)
                     user.solved_problems = solved_problems
                     user.solved_count = userdata['solvedCount']
-                await asyncio.sleep(10)
+                await asyncio.sleep(20)
             
-            await asyncio.sleep(120)
+            await asyncio.sleep(300)
 
     def stop(self):
         self.task.cancel()
